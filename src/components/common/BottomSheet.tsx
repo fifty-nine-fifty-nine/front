8796f2ec101bc875c22cbe77c798b9a7c5fe9ece
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-03 18:11:56 +0900

    feat: 바텀시트 구현 및 알러지 리스트 추가

--- src/components/common/BottomSheet.tsx
+++ src/components/common/BottomSheet.tsx
@@ -23,7 +23,7 @@ const BottomSheet: React.FC<BottomSheetProps> = ({ isOpen, onClose, children, ti
         }`,
       )}
     >
-      <div className="flex flex-col max-w-2xl  h-full rounded-t-xl bg-slate-400">
+      <div className="flex flex-col max-w-2xl  h-full rounded-t-xl bg-white">
         <div className={cn(flexBetween, `h-[66px] border-b border-neutral-300 px-5`)}>
           <h2 className={titleMd}>{title}</h2>
           <button className={'w-6 h-6'} onClick={onClose}>
@@ -37,18 +37,18 @@ const BottomSheet: React.FC<BottomSheetProps> = ({ isOpen, onClose, children, ti
             />
           </button>
         </div>
-        <div className={`px-5 pt-8`}>{children}</div>
-        <footer
-          className={cn(
-            flexColCenter,
-            `absolute max-w-2xl left-0 bottom-0 right-0 px-5 pt-3 h-24 `,
-          )}
-        >
-          <button className={cn(button(), 'bottom-8')}>
-            <p className={whiteText}>확인</p>
-          </button>
-        </footer>
+        <div className={`px-5 pt-8 pb-32 overflow-y-auto`}>{children}</div>
       </div>
+      <footer
+        className={cn(
+          flexColCenter,
+          `z-20	absolute max-w-2xl left-0 bottom-0 right-0 px-5 pt-3 h-24 bg-white`,
+        )}
+      >
+        <button className={cn(button(), 'bottom-8')} onClick={onClose}>
+          <p className={whiteText}>확인</p>
+        </button>
+      </footer>
     </div>
   );
 };

89b60576609ee8aee8f0b656f5882302c93a9c54
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-04 20:13:20 +0900

    feat: 입력창 3개까지 늘릴수 있는 기능 추가

--- src/components/businesscard/BusinessCardPreferenceView.tsx
+++ src/components/businesscard/BusinessCardPreferenceView.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import { GenerateItem, GenerateView } from '@/components';
 import { button, input } from '@/styles/ogoo';
 import { flexCol } from '@/styles/ogoo/alignment.css';
@@ -8,6 +10,23 @@ import { subtitleMd } from '@/styles/ogoo/typography.css';
 import { cn } from '@/utils';
 
 export default function BusinessCardPreferenceView() {
+  const [likeInputCount, setLikeInputCount] = useState(1); // Initialize with 1 input
+  const [unlikeInputCount, setUnLikeInputCount] = useState(1); // Initialize with 1 input
+
+  const maxInputCount = 3;
+
+  const handleAddLikeInput = () => {
+    if (likeInputCount < 3) {
+      setLikeInputCount(likeInputCount + 1); // Increase the input count, but limit it to 3
+    }
+  };
+
+  const handleAddUnLikeInput = () => {
+    if (unlikeInputCount < 3) {
+      setUnLikeInputCount(unlikeInputCount + 1); // Increase the input count, but limit it to 3
+    }
+  };
+
   return (
     <GenerateView
       questionNumber={'5'}
@@ -16,17 +35,35 @@ export default function BusinessCardPreferenceView() {
     >
       <div className={cn(flexCol, 'px-5 gap-8')}>
         <GenerateItem question={'말랑이는 이런 것들을 좋아해요.'}>
-          <input className={cn(input())} placeholder="ex) 산책을 좋아해요" />
-          <button className={cn(button({ color: 'sub', size: 'sm' }))}>
-            <p className={cn(subtitleMd, subtitleText)}>+</p>
-          </button>
+          {[...Array(likeInputCount)].map((_, index) => (
+            <div key={index} className="flex gap-3">
+              <input className={cn(input())} placeholder="ex) 산책을 좋아해요" />
+            </div>
+          ))}
+          {likeInputCount < maxInputCount && (
+            <button
+              className={cn(button({ color: 'sub', size: 'sm' }))}
+              onClick={handleAddLikeInput}
+            >
+              <p className={cn(subtitleMd, subtitleText)}>+</p>
+            </button>
+          )}
         </GenerateItem>
 
         <GenerateItem question={'말랑이에게 이런 점들을 주의해주세요.'}>
-          <input className={cn(input())} placeholder="ex) 손 내밀기 금지" />
-          <button className={cn(button({ color: 'sub', size: 'sm' }))}>
-            <p className={cn(subtitleMd, subtitleText)}>+</p>
-          </button>
+          {[...Array(unlikeInputCount)].map((_, index) => (
+            <div key={index} className="flex gap-3">
+              <input className={cn(input())} placeholder="ex) 손 내밀기 금지" />
+            </div>
+          ))}
+          {unlikeInputCount < maxInputCount && (
+            <button
+              className={cn(button({ color: 'sub', size: 'sm' }))}
+              onClick={handleAddUnLikeInput}
+            >
+              <p className={cn(subtitleMd, subtitleText)}>+</p>
+            </button>
+          )}
         </GenerateItem>
       </div>
     </GenerateView>

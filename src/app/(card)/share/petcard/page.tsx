450e39f33866b14316086852186ace86577c43fa
Sooyoung Yoon <ayoooo240@gmail.com>
2023-09-13 02:46:28 +0900

    feat: 결과 공유 페이지를 위한 share 라우트 추가

---
+++ src/app/(card)/share/petcard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SharePetCardPage = () => {
+  // TODO: URL query string으로 type, name, img_url 넘겨받기
+  // URL에 정보가 담겨있어야 주소 공유 시 렌더 가능
+
+  return <div>펫카드 공유 페이지</div>;
+};
+
+export default SharePetCardPage;

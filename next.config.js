b5ef4791e2fb2441c5e6f75eb5d08421dea3004e
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-09-16 01:56:41 +0900

    fix: test to rollback next config js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
-const withVanillaExtract = createVanillaExtractPlugin();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['k.kakaocdn.net'],
-  },
-};
+const nextConfig = {}
 
-module.exports = withVanillaExtract({
-  ...nextConfig,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
-});
+module.exports = nextConfig

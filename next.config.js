00b0909b787340d947b0aefacdc5fa57865199f2
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-09-16 01:02:02 +0900

    Revert "fix: Next Config js"
    This reverts commit 15872212148fc2f514ff22bc252290e6237489c8.

--- next.config.js
+++ next.config.js
@@ -17,21 +17,6 @@ module.exports = withVanillaExtract({
       use: ['@svgr/webpack'],
     });
 
-    // Add esbuild loader
-    config.module.rules.push({
-      test: /\.(tsx|ts|js|mjs|jsx)$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: 'esbuild-loader',
-          options: {
-            loader: 'tsx',  // or 'ts' if you don't need tsx
-            target: 'es2017',
-          },
-        },
-      ],
-    });
-
     return config;
   },
 });

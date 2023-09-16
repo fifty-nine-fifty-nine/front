c69adbe02ea12fbe0d9169c82cb44bc49f43c56f
Sooyoung Yoon <ayoooo240@gmail.com>
2023-09-16 14:53:25 +0900

    fix: url에 description 추가

--- src/app/(card)/share/petcard/component/PetCardResult.tsx
+++ src/app/(card)/share/petcard/component/PetCardResult.tsx
@@ -20,26 +20,30 @@ export const PetCardResult = ({ petCardInfo }: Props) => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
-  const descriptionRef = useRef<string | null>();
 
   useEffect(() => {
     if (searchParams.get('type')) {
       const name = petCardInfo.name;
-      const imgPath = petCardInfo.img_url.split('/').slice(4).join('/');
-      descriptionRef.current = searchParams.get('description');
-      router.replace(`${pathname}?name=${name}&imgPath=${encodeURIComponent(imgPath)}`);
+      const path = petCardInfo.img_url.split('/').slice(4).join('/');
+      const description = searchParams.get('description')?.replaceAll('%20', ' ') ?? '';
+
+      const replaceUrl = `${pathname}?name=${name}&path=${encodeURIComponent(
+        path,
+      )}&desc=${encodeURIComponent(description)}`;
+
+      router.replace(replaceUrl);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router, searchParams]);
 
-  if (searchParams.get('imgPath')) {
+  if (searchParams.get('path')) {
     return (
       <>
         <header className={cn(`absolute top-0 left-0 right-0 pt-12 bg-white w-full`)}>
           <StepBackButton />
         </header>
 
-        <div className="h-full flex-col justify-between px-5">
+        <div className="h-full flex-col justify-between px-5 mt-[-40px]">
           <h2 className={cn(titleLg, `mb-3 text-center`)}>펫 카드가 만들어졌어요!</h2>
 
           <article
@@ -50,7 +54,7 @@ export const PetCardResult = ({ petCardInfo }: Props) => {
           >
             <picture className={`w-[400px] h-[274px] relative`}>
               <Image
-                src={`${IMAGE_BASE_URL}${searchParams.get('imgPath')}`}
+                src={`${IMAGE_BASE_URL}${searchParams.get('path')}`}
                 width={350}
                 height={350}
                 alt={`${searchParams.get('name')}의 펫카드`}
@@ -60,7 +64,7 @@ export const PetCardResult = ({ petCardInfo }: Props) => {
 
             <div className={cn(bgSub, `sticky bottom-0 left-0 right-0 w-full p-5`)}>
               <strong className="block mb-1">{searchParams.get('name')}</strong>
-              <p className={subText}>{descriptionRef.current}</p>
+              <p className={subText}>{decodeURIComponent(searchParams.get('desc') ?? '')}</p>
             </div>
           </article>
 
@@ -72,7 +76,7 @@ export const PetCardResult = ({ petCardInfo }: Props) => {
             </button>
           </div>
 
-          <div className={`w-full px-1 my-10`}>
+          <div className={`w-full px-1 py-10`}>
             <strong className={cn(bodyLg, subText, `block pb-2`)}>유의사항</strong>
             <ul className={bulletItem}>
               <li>펫 카드는 생성된 후 마이페이지에 저장되지 않습니다.</li>

6c44a9d2cfa60bedb539b5fb68b0400e8e3e83d0
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-21 16:21:10 +0900

    setting: maze 코드 삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/tailwind.css';
 
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+import Script from 'next/script';
 
 import { themeClass } from '@/styles';
 import { flexCenter } from '@/styles/ogoo/alignment.css';
@@ -24,6 +25,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body className={cn(themeClass, Pretendard.className, flexCenter, bgSubPrimary)}>
+        <Script
+          id="maze-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function (m, a, z, e) {
+                var s, t;
+                try {
+                  t = m.sessionStorage.getItem('maze-us');
+                } catch (err) {}
+
+                if (!t) {
+                  t = new Date().getTime();
+                  try {
+                    m.sessionStorage.setItem('maze-us', t);
+                  } catch (err) {}
+                }
+
+                s = a.createElement('script');
+                s.src = z + '?t=' + t + '&apiKey=' + e;
+                s.async = true;
+                a.getElementsByTagName('head')[0].appendChild(s);
+                m.mazeUniversalSnippetApiKey = e;
+              })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '97e150d1-30f7-4e8b-9568-3037fb36dce3');
+            `,
+          }}
+        />
         <main
           className={cn(mainContainer, `flex-1 max-w-md relative overflow-hidden drop-shadow-sm`)}
         >

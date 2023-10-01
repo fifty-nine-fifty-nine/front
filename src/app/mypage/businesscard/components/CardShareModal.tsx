9d0397c202ae6e428123c9fbeacae74eaf7d6f5d
Sooyoung Yoon <ayoooo240@gmail.com>
2023-10-01 12:01:45 +0900

    fix: domain window 관련 에러 해결

--- src/app/mypage/businesscard/components/CardShareModal.tsx
+++ src/app/mypage/businesscard/components/CardShareModal.tsx
@@ -1,7 +1,7 @@
 import type { ModalProps } from '@/components/common/PopupModal';
 import PopupModal from '@/components/common/PopupModal';
 import { ShareButtonList } from '@/components/common/ShareButtonList';
-import { domainExtractor } from '@/utils';
+import { useDomain } from '@/hooks/useDomain';
 
 interface Props extends ModalProps {
   petName: string;
@@ -16,26 +16,26 @@ export const CardShareModal = ({
   imageUrl,
   selectedShareThumb,
 }: Props) => {
-  const currentUrl = window.location.href;
-  const domain = domainExtractor(currentUrl, '/share');
+  const domain = useDomain();
 
-  return (
-    <PopupModal
-      isOpen={isOpen}
-      closeModal={closeModal}
-      title={`${petName} 명함을 공유할까요?`}
-      content={`명함 공유시, 집사님 외 다른 사람들은\n저장이 불가능합니다.`}
-      actions={[
-        <ShareButtonList
-          key="share"
-          templateId={98893}
-          requestUrl={`petName=${petName}&frontPage=${imageUrl[0]}&backPage=${imageUrl[1]}`}
-          thumbImgPath={selectedShareThumb}
-          petName={petName}
-          imageUrl={imageUrl}
-          domain={domain}
-        />,
-      ]}
-    />
-  );
+  if (domain)
+    return (
+      <PopupModal
+        isOpen={isOpen}
+        closeModal={closeModal}
+        title={`${petName} 명함을 공유할까요?`}
+        content={`명함 공유시, 집사님 외 다른 사람들은\n저장이 불가능합니다.`}
+        actions={[
+          <ShareButtonList
+            key="share"
+            templateId={98893}
+            requestUrl={`petName=${petName}&frontPage=${imageUrl[0]}&backPage=${imageUrl[1]}`}
+            thumbImgPath={selectedShareThumb}
+            petName={petName}
+            imageUrl={imageUrl}
+            domain={domain}
+          />,
+        ]}
+      />
+    );
 };
